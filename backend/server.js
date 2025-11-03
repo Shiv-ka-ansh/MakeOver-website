39dffbaf12f1901b81f5ebefa9a88da9cda3293f
Shivansh Gupta <shivanshg2002@gmail.com>
2025-11-03 15:31:57 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ const app = express();
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 
-app.use(cors());
+app.use(cors( {
+    origin: [ " https://makeover-website-1.onrender.com", "http://localhost:5000"] ,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 app.use(express.json());
 
 // Health check endpoint
@@ -103,5 +107,5 @@ Message: ${sanitizedMessage}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port} \nhttp://localhost:${port}`);
+    console.log(`Server listening on port ${port} \n http://localhost:${port}`);
 })
